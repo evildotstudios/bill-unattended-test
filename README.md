5c70de1d1b2d2952545d027ee8e720d22e6d0448
Ricardo <ricardovxuk@gmail.com>
2015-06-11 08:55:07 +0100

    Readme updated

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # Bill unattended test
 
+## Premise
+The major intent is to build a small app that fetches and shows a Sky bill. Also the objective is to show a simple and clean solution, with TDD usage. 
+
+
 ## Installation
+I've included the vendor directory in the repo (usually I don't) to make it easier to make it run
+
 
 1. clone the code to any web directory 
-2. composer update
 3. open in browser
 
 
+# Testing
+Tests can be run at the root of the cloned solution with the command:
 
+./vendor/bin/phpunit SanitizeTest.php
+
+Like I've told in the previous interview, I've never used unit testing in depth but it is a demonstrable test
 
--- SanitizeTest.php
+++ SanitizeTest.php
@@ -5,21 +5,33 @@
 */
 class SanitizeTest extends PHPUnit_Framework_TestCase
 {
-	var $sanitize;
-	var $content;
+
+	var $path;
 
 	public function __construct()
 	{
-		$this->sanitize = new \Sanitize;
-		$this->content = \Content::getContent();
+		$this->path = realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR;
 	}
+
 	public function testdoSanitize()
 	{
+		$sanitize = new \Sanitize;
+		$content = new \Content;
 
+		$source = $content::getContent($this->path.'bill.json');
+
+
+
+		$test = $sanitize::doSanitize($source);
+
+		$this->assertNotEmpty($test);
+		$this->assertTrue(is_object($test));
+
+		$this->assertObjectHasAttribute('statement',$test);
+		$this->assertObjectHasAttribute('total',$test);
+		$this->assertObjectHasAttribute('package',$test);
+		$this->assertObjectHasAttribute('callCharges',$test);
+		$this->assertObjectHasAttribute('skyStore',$test);
 	}
 
-	public function testpaginateCalls()
-	{
-		
-	}
 }
